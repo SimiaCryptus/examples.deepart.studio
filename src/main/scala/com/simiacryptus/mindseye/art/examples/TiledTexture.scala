ab60c2633ebe612c2ffe99ef5dcc1d7a7efd8f9c
Andrew Charneski <acharneski@gmail.com>
2019-08-25 12:05:48 -0700

    wip

--- src/main/scala/com/simiacryptus/mindseye/art/examples/TiledTexture.scala
+++ src/main/scala/com/simiacryptus/mindseye/art/examples/TiledTexture.scala
@@ -26,34 +26,38 @@ import com.simiacryptus.mindseye.art.models.VGG16
 import com.simiacryptus.mindseye.art.ops._
 import com.simiacryptus.mindseye.art.util.ArtSetup.{ec2client, s3client}
 import com.simiacryptus.mindseye.art.util.{BasicOptimizer, _}
-import com.simiacryptus.mindseye.eval.Trainable
 import com.simiacryptus.mindseye.lang.Tensor
 import com.simiacryptus.mindseye.layers.java.{ImgTileAssemblyLayer, ImgViewLayer}
-import com.simiacryptus.mindseye.opt.Step
 import com.simiacryptus.notebook.NotebookOutput
 import com.simiacryptus.sparkbook.NotebookRunner.withMonitoredJpg
 import com.simiacryptus.sparkbook._
 import com.simiacryptus.sparkbook.util.Java8Util._
 import com.simiacryptus.sparkbook.util.LocalRunner
 
-import scala.util.Try
+object TiledTexture extends TiledTexture with LocalRunner[Object] with NotebookRunner[Object]
 
-object TextureTiled extends TextureTiled with LocalRunner[Object] with NotebookRunner[Object]
-
-class TextureTiled extends ArtSetup[Object] {
+class TiledTexture extends ArtSetup[Object] {
 
   val styleUrl = "upload:Style"
-  val initUrl: String = "50 + plasma * 0.5"
+  val initUrl: String = "50 + noise * 0.5"
   val s3bucket: String = "examples.deepartist.org"
-  val minResolution = 200
-  val maxResolution = 512
-  val magnification = 4
+  val minResolution = 128
+  val maxResolution = 400
+  val magnification = 9
   val rowsAndCols = 3
+
   override def indexStr = "201"
 
   override def description =
     """
-      |Creates a tiled texture based on a style.
+      |Creates a simple tiled texture based on a style using:
+      |<ol>
+      |<li>Random noise initialization</li>
+      |<li>Standard VGG16 layers</li>
+      |<li>Operators constraining and enhancing style</li>
+      |<li>Progressive resolution increase</li>
+      |<li>View layer to enforce tiling</li>
+      |</ol>
       |""".stripMargin.trim
 
   override def inputTimeoutSeconds = 3600
@@ -65,6 +69,7 @@ class TextureTiled extends ArtSetup[Object] {
       log.out(log.jpg(ImageArtUtil.load(log, styleUrl, (maxResolution * Math.sqrt(magnification)).toInt), "Input Style"))
       val canvas = new AtomicReference[Tensor](null)
 
+      // Generates a pretiled image (e.g. 3x3) to display
       def tiledCanvas = {
         val input = canvas.get()
         if (null == input) input else {
@@ -75,59 +80,55 @@ class TextureTiled extends ArtSetup[Object] {
         }
       }
 
-      def calcFn(dims: Seq[Int]) = {
+      // Tiling layer used by the optimization engine.
+      // Expands the canvas by a small amount, using tile wrap to draw in the expanded boundary.
+      def tilingLayer(dims: Seq[Int]) = {
         val padding = Math.min(256, Math.max(16, dims(0) / 2))
         new ImgViewLayer(dims(0) + padding, dims(1) + padding, true)
           .setOffsetX(-padding / 2).setOffsetY(-padding / 2)
       }
 
+      // Execute the main process while registered with the site index
       val registration = registerWithIndexJPG(tiledCanvas)
-      NotebookRunner.withMonitoredJpg(() => tiledCanvas.toImage) {
-        try {
+      try {
+        // Display a pre-tiled image inside the report itself
+        withMonitoredJpg(() => tiledCanvas.toImage) {
+          // Display an additional, non-tiled image of the canvas
           withMonitoredJpg(() => Option(canvas.get()).map(_.toRgbImage).orNull) {
-            var steps = 0
-            Try {
-              log.subreport("Painting", (sub: NotebookOutput) => {
-                paint(styleUrl, initUrl, canvas, new VisualStyleNetwork(
-                  styleLayers = List(
-                    VGG16.VGG16_0,
-                    VGG16.VGG16_1a,
-                    VGG16.VGG16_1b1,
-                    VGG16.VGG16_1b2,
-                    VGG16.VGG16_1c1,
-                    VGG16.VGG16_1c2,
-                    VGG16.VGG16_1c3
-                  ),
-                  styleModifiers = List(
-                    new GramMatrixEnhancer(),
-                    new MomentMatcher()
-                  ),
-                  styleUrl = List(styleUrl),
-                  magnification = magnification,
-                  viewLayer = calcFn
-                ), new BasicOptimizer {
-                  override val trainingMinutes: Int = 60
-                  override val trainingIterations: Int = 30
-                  override val maxRate = 1e9
-
-                  override def onStepComplete(trainable: Trainable, currentPoint: Step): Boolean = {
-                    steps = steps + 1
-                    super.onStepComplete(trainable, currentPoint)
-                  }
-                }, new GeometricSequence {
-                  override val min: Double = minResolution
-                  override val max: Double = maxResolution
-                  override val steps = 2
-                }.toStream.map(_.round.toDouble): _*)(sub)
-                null
-              })
-            }
-            uploadAsync(log)
+            log.subreport("Painting", (sub: NotebookOutput) => {
+              paint(styleUrl, initUrl, canvas, new VisualStyleNetwork(
+                styleLayers = List(
+                  VGG16.VGG16_0,
+                  VGG16.VGG16_1a,
+                  VGG16.VGG16_1b1,
+                  VGG16.VGG16_1b2,
+                  VGG16.VGG16_1c1,
+                  VGG16.VGG16_1c2,
+                  VGG16.VGG16_1c3
+                ),
+                styleModifiers = List(
+                  new GramMatrixEnhancer(),
+                  new MomentMatcher()
+                ),
+                styleUrl = List(styleUrl),
+                magnification = magnification,
+                viewLayer = tilingLayer
+              ), new BasicOptimizer {
+                override val trainingMinutes: Int = 60
+                override val trainingIterations: Int = 15
+                override val maxRate = 1e9
+              }, new GeometricSequence {
+                override val min: Double = minResolution
+                override val max: Double = maxResolution
+                override val steps = 2
+              }.toStream.map(_.round.toDouble): _*)(sub)
+              null
+            })
           }(log)
-          null
-        } finally {
-          registration.foreach(_.stop()(s3client, ec2client))
         }
+        null
+      } finally {
+        registration.foreach(_.stop()(s3client, ec2client))
       }
     }
   }()
