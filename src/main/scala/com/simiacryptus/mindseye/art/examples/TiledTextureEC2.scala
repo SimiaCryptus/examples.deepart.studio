c180a1c6235eb82b45b7f239640a5173cf1c1776
Andrew Charneski <acharneski@gmail.com>
2020-03-14 22:21:22 -0700

    Updated test and notebook classes

---
+++ src/main/scala/com/simiacryptus/mindseye/art/examples/TiledTextureEC2.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 by Andrew Charneski.
+ *
+ * The author licenses this file to you under the
+ * Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.simiacryptus.mindseye.art.examples
+
+import com.simiacryptus.mindseye.art.aws.P2_XL
+
+object TiledTextureEC2 extends TiledTexture with P2_XL {
+
+  override val s3bucket: String = "examples.deepartist.org"
+
+  override def className: String = "TiledTexture"
+
+}
